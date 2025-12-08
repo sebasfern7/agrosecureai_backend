7e9e016edd1f4050f0dfb8878fd7418ad0f8998b
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-12-08 00:30:19 -0400

    Fix: Add missing UpdateFarmInput DTO

---
+++ src/main/java/com/agrosecure/dto/UpdateFarmInput.java
@@ -0,0 +1,12 @@
+package com.agrosecure.dto;
+
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class UpdateFarmInput {
+    private UUID id;
+    private String name;
+    private String location;
+}
+
